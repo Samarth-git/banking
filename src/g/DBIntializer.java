6effa2c9eba1135143c3824d255bf7bbc906612d
samarth <gupta.samarth25@gmail.com>
2020-08-21 18:51:44 +0530

    first commit

---
+++ src/g/DBIntializer.java
@@ -0,0 +1,9 @@
+package g;
+
+public interface DBIntializer {
+	String DRIVER="com.mysql.jdbc.Driver";
+	String CON_STRING="jdbc:mysql://localhost:3306/sammy";
+	String USERNAME="root";
+	String PASSWORD="root";
+
+}
